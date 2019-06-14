980320921b861c1dd2a376f3bfb6b4e68bf85cfd
nawww83 <nawww83@gmail.com>
2019-06-14 16:05:00 +0700

    - Рефакторинг кода. - Введено условие выхода из цикла, ускоряющее поиск наилучшего порождающего полинома. Условие основано на предварительной сортировке полиномов по убыванию их веса. Если вес текущего полинома меньше или равен текущему найденному максимальному расстоянию, то считать поиск законченным, так как остались полиномы с меньшим весом и они заведомо не дадут код с кодовым расстоянием, превышающим найденное.

--- gen_poly.py
+++ gen_poly.py
@@ -14,11 +14,11 @@ def expandMultipleFactor(factors):
     ''' Представляет полином f(x)**k в виде k полиномов f(x) '''
     ''' Например, (x**2 + 1)**3 => [(x**2 + 1), (x**2 + 1), (x**2 + 1)]'''
     ''' Для упрошения логики работы последующих функций'''
-    result = []        
+    result = []
     for f in factors:
         fb = f.as_base_exp()
         for m in range(fb[1]):
-            result.append(fb[0])   
+            result.append(fb[0])
     return result
 
 def getFactorsFromMaxima(n):
@@ -31,7 +31,7 @@ def getFactorsFromMaxima(n):
         + ns + \
         r' + 1), modulus:2; stringout(\"factor.txt\", %o1);quit();"'
     with open(os.devnull, "w") as f:
-        ret_code = subprocess.call(cmd, shell=True, stdout=f)    
+        ret_code = subprocess.call(cmd, shell=True, stdout=f)
     if ret_code:
         return []
 
@@ -66,7 +66,7 @@ def doFactors(n):
 
     return expandMultipleFactor(factors)
 
-def findGenPolys(factors, r):
+def composeGenPolys(factors, r):
     candidates = set()
     for n_factors in range(1, len(factors) + 1):
         for combs in it.combinations(factors, n_factors):
@@ -79,9 +79,10 @@ def findGenPolys(factors, r):
     return list(candidates)
 
 def withoutMirrors(polys):
+    ''' Фильтрует зеркальные полиномы '''
     result = []
     uniq = []
-    for p in polys:        
+    for p in polys:
         cl = p.as_poly().all_coeffs()
         cl_mirror = cl[::-1]
         if cl not in uniq:
@@ -121,25 +122,45 @@ def getSpectrum(gen_poly, n, r):
             a_x = sy.Poly.from_list(a_, x, modulus = 2)
             d_ = sy.expand(a_x * g_x)
             sp[d_.count(1) + d_.count(x)] += 1
-    # remove items with zero values
+    # удаление элементов с нулевым значением
     filtered = {k: v for k, v in sp.items() if v > 0}
     sp.clear()
     sp.update(filtered)
-    # sort dictionary by key
+    # сортировка словаря по ключу
     result = OrderedDict()
     for key in sorted(sp, key = lambda i: i):
         result.update({key: sp[key]})
     return sorted(result.items(), key = lambda x: x[0])
+    
+def weightPoly(p):
+    ''' Возвращает хеммингов вес полинома'''
+    return p.count(1) + p.count(x)
+
+def sortPolys(polys, desc = True):
+    ''' Сортирует полиномы в порядке убывания их веса'''
+    weights = [weightPoly(p) for p in polys]
+    indexies = sorted(range(len(weights)), \
+        key = weights.__getitem__, \
+        reverse = desc)
+    return [polys[i] for i in indexies]
 
 def findTheBestPoly(polys, n, r):
-    '''Возвращает порождающий полином с максимальным кодовым растоянием'''
+    ''' Возвращает порождающий полином с максимальным кодовым растоянием '''
+    ''' Полиномы должны быть отсортированы в порядке убывания их веса '''
     n_polys = len(polys)
-    spectrums = [getSpectrum(polys[x], n, r) for x in range(n_polys)]
-    d_codes = [spectrums[x][1][0] for x in range(n_polys)]
-    bestDistance = max(d_codes)
-    position = d_codes.index(bestDistance)
-    bestPoly = polys[position] 
-    return bestPoly, spectrums[position], d_codes
+    max_d = 0
+    for p in polys:
+        weight = weightPoly(p)
+        sp = getSpectrum(p, n, r)
+        d = sp[1][0]
+        if d > max_d: # нашли лучший код
+            bestPoly = p
+            max_d = d
+            good_sp = sp
+        if weight <= max_d: # нет смысла искать лучший полином среди 
+            # полиномов, вес которых меньшн текущего наибольшего расстояния кода
+            break
+    return bestPoly, good_sp, max_d
 
 def getCheckPoly(gen_poly, n):
     ''' Возвращает проверочный полином h(x) '''
@@ -159,27 +180,28 @@ if __name__== "__main__":
     print('Факторизация нуль-полинома (x^{}+1) над полем GF(2)...'.format(n))
 
     factors = doFactors(n)
-    print('Поиск порождающих полиномов степени r={}...'.format(r))
-    gen_polys = findGenPolys(factors, r)
+    print('Формирование порождающих полиномов степени r={}...'.format(r))
+    gen_polys = composeGenPolys(factors, r)
+    print('Фильтрация зеркальных полиномов...')
     gen_polys = withoutMirrors(gen_polys)
+    print('Сортировка полиномов в порядке убывания их веса...')
+    gen_polys = sortPolys(gen_polys)
     if not gen_polys:
         print('Порождающего полинома степени r={} не найдено!'.format(r))
         exit()
     else:
-        print('Найденные порождающие полиномы степени r={} за исключением зеркальных'.format(r))
+        print('Найденные порождающие полиномы')
         for p in gen_polys:
             print(p)
-    print('Выбор лучшего полинома по критерию наибольшего кодового расстояния...')
-    good_poly, spectrum, d_codes = findTheBestPoly(gen_polys, n, r)
-    bestDistance = max(d_codes)
-    print('Список кодовых расстояний соответствующих циклических кодов')
-    print(d_codes)
-    print('Порождающий полином g(x), дающий наибольшее кодовое расстояние, равное {}'.format(bestDistance))
+    print('Поиск наилучшего полинома по критерию максимального кодового расстояния...')
+    good_poly, spectrum, bestDistance = findTheBestPoly(gen_polys, n, r)
+    print('Наилучший порождающий полином g(x)')
     print(good_poly)
+    print('Кодовое расстояние {}'.format(bestDistance))
 
-    gen_matrix = getGenMatrix(good_poly, n, r)
-    print('Порождающая матрица G кода ({}, {})'.format(n, n - r))
-    print(gen_matrix)
+    #gen_matrix = getGenMatrix(good_poly, n, r)
+    #print('Порождающая матрица G кода ({}, {})'.format(n, n - r))
+    #print(gen_matrix)
     check_poly = getCheckPoly(good_poly, n)
     print('Проверочный полином h(x)')
     print(check_poly)
